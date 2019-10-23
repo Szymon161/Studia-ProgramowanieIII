f295d50cc2f5ecc5053dd583022841498077497d
Szymon Świstek <szymonswistek6@gmial.com>
2019-10-23 21:25:04 +0200

    Programy na studia

---
+++ Laboratorium4/Funkcje/Funkcje/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tablica = { "AAA", "BBB", "CCC", "DDD", "EEE" };
+            
+            int[,] tablicaDwuwymiarowa =
+            { 
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 }
+            };
+
+            int[][] tablicaPoszarpana = 
+            {
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                new int[] { 1, 2, 3, 4, 5, 6, 7 },
+                new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 1, 2, 3 },
+                new int[] { 1 }
+            };
+
+            Console.Write("Tablica:\t");
+            Tablice<string>.WypiszTablice(tablica);
+
+            Console.Write("Kopia Tablicy:\t");
+            Tablice<string>.WypiszTablice(Tablice<string>.KopiowanieTablicy(tablica));
+
+            Console.Write("Tablica Dwuwymiarowa:\t\t");
+            Tablice<int>.WypiszTablice(tablicaDwuwymiarowa);
+
+            Console.Write("Kopia Tablicy Dwuwymiarowej:\t");
+            Tablice<int>.WypiszTablice(Tablice<int>.KopiowanieTablicyDwuwymiarowej(tablicaDwuwymiarowa));
+
+            Console.Write("Tablica Poszarpana:\t\t");
+            Tablice<int>.WypiszTablice(tablicaPoszarpana);
+
+            Console.Write("Kopia Tablicy Poszarpanej:\t");
+            Tablice<int>.WypiszTablice(Tablice<int>.KopiowanieTablicyPoszarpanej(tablicaPoszarpana));
+
+            int liczba1 = 1;
+            int liczba2 = 2;
+
+            Fun(liczba1);
+            FunRef(ref liczba2);
+            FunOut(out int liczba3);
+
+            Console.WriteLine($"Liczba1: {liczba1}\nLiczba2: {liczba2}\nLiczba3: {liczba3}");
+
+            Console.ReadKey();
+        }
+
+        static void Fun(int n)
+        {
+            n = 456;
+        }
+
+        static void FunRef(ref int n)
+        {
+            n = 456;
+        }
+
+        static void FunOut(out int n)
+        {
+            n = 456;
+        }
+    }
+}
---
+++ Laboratorium4/Funkcje/Funkcje/Tablice.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Funkcje
+{
+    static class Tablice<T>
+    {
+        internal static void WypiszTablice(T[] tablica)
+        {
+            foreach (T item in tablica)
+            {
+                Console.Write(item.ToString() + ' ');
+            }
+            Console.WriteLine();
+        }
+
+        internal static void WypiszTablice(T[,] tablica)
+        {
+            for (int i = 0; i < tablica.GetLength(1); i++)
+            {
+                for (int j = 0; j < tablica.GetLength(0); j++)
+                {
+                    Console.Write(tablica[i, j].ToString() + ' ');
+                }
+            }
+            Console.WriteLine();
+        }
+
+        internal static void WypiszTablice(T[][] tablica)
+        {
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                for (int j = 0; j < tablica[i].Length; j++)
+                {
+                    Console.Write(tablica[i][j].ToString() + ' ');
+                }
+            }
+            Console.WriteLine();
+        }
+
+        internal static T[] KopiowanieTablicy(T[] tablicaOryginalna)
+        {
+            var kopia = new T[tablicaOryginalna.Length];
+
+            for (int i = 0; i < tablicaOryginalna.Length; i++)
+            {
+                kopia[i] = tablicaOryginalna[i];
+            }
+
+            return kopia;
+        }
+
+        internal static T[,] KopiowanieTablicyDwuwymiarowej(T[,] tablicaOryginalna)
+        {
+            var kopia = new T[tablicaOryginalna.GetLength(1), tablicaOryginalna.GetLength(0)];
+
+            for (int i = 0; i < tablicaOryginalna.GetLength(1); i++)
+            {
+                for (int j = 0; j < tablicaOryginalna.GetLength(0); j++)
+                {
+                    kopia[i, j] = tablicaOryginalna[i, j];
+                }
+            }
+
+            return kopia;
+        }
+
+        internal static T[][] KopiowanieTablicyPoszarpanej(T[][] tablicaOryginalna)
+        {
+            var kopia = new T[tablicaOryginalna.Length][];
+
+            for (int i = 0; i < tablicaOryginalna.Length; i++)
+            {
+                kopia[i] = new T[tablicaOryginalna[i].Length];
+
+                for (int j = 0; j < tablicaOryginalna[i].Length; j++)
+                {
+                    kopia[i][j] = tablicaOryginalna[i][j];
+                }
+            }
+
+            return kopia;
+        }
+    }
+}
