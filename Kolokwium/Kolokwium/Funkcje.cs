dacab1c8724bb21b260c921fa2f807020c3f3123
Szymon Świstek <szymonswistek6@gmial.com>
2019-11-10 16:26:58 +0100

    Pierwsze kolokwium

---
+++ Kolokwium/Kolokwium/Funkcje.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium
+{
+    class Funkcje
+    {
+        public static bool CzyPunktNalezyDoWykresu(double x, double y)
+        {
+            return y == 0.5 * x * x - 10 * x + 1;
+        }
+
+        //2D
+        public string SzyfrCezara(string napis, int iloscZnakow)
+        {
+            StringBuilder sb = new StringBuilder(napis);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                int wartosc = sb[i] + iloscZnakow;
+                sb[i] = (char)wartosc;
+            }
+
+            return sb.ToString();
+        }
+
+        //3D
+        public void PrzeszukiwanieTablicyPoszarpanej(int[][] tablica, int parametr)
+        {
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                for (int j = 0; j < tablica[i].Length; j++)
+                {
+                    if (tablica[i][j] == parametr)
+                    {
+                        Console.WriteLine("Indeks tablicy: [" + i + "][" + j + "]");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kolokwium/Kolokwium/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Kolokwium
+{
+    //grupa D
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[][] tablica =
+            {
+                new[] { 3, 4 },
+                new[] { 2, 6, 1, },
+                new[] { 11, 5, 123, }
+            };
+
+            Console.WriteLine(Funkcje.CzyPunktNalezyDoWykresu(0, 1));
+
+            var f = new Funkcje();
+
+            Console.WriteLine(f.SzyfrCezara("abcDEF", 1));
+
+            f.PrzeszukiwanieTablicyPoszarpanej(tablica, 6);
+
+            Console.ReadKey();
+        }
+    }
+}
