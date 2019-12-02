952c618c01703de4fcb5a3a280d1cc9c913ed291
Szymon Świstek <szymonswistek6@gmial.com>
2019-12-02 13:16:29 +0100

    Update

---
+++ Laboratorium6/Laboratorium6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using RandomDataGenerator.Randomizers;
+using RandomDataGenerator.FieldOptions;
+
+namespace Laboratorium6
+{
+    class Person
+    {
+        public int Id { get; }
+        public string FirstName { get; }
+        public string Surname { get; }
+
+        public Person(int id, string imie, string nazwisko)
+        {
+            Id = id;
+            FirstName = imie;
+            Surname = nazwisko;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {Surname}, {Id}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var idGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsInteger());
+            var firstNameGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
+            var lastNameGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsLastName());
+
+            var people = Enumerable.Range(1, 100)
+                .Select(x => new Person(idGenerator.Generate().Value, firstNameGenerator.Generate(), lastNameGenerator.Generate()))
+                .ToList();
+            
+            Console.WriteLine($"Number of people: {people.Count()}\nID's average: {people.Select(x => x.Id).Average()}\n");
+
+            people.Where(x => x.Surname[0] == 'B' || x.Surname[0] == 'C' || x.FirstName[0] == 'J')
+                .Select(x => x.Surname + ' ' + x.FirstName)
+                .OrderBy(x => x)
+                .ThenBy(x => x)
+                .ToList()
+                .ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+    }
+}
