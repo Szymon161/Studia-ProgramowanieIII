58bc509ef529752d0300edf4ae9999250bfcb891
Szymon Świstek <szymonswistek6@gmial.com>
2020-01-15 19:40:58 +0100

    Pierwszy projekt

---
+++ Laboratorium1/PierwszyProjekt/PierwszyProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PierwszyProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
