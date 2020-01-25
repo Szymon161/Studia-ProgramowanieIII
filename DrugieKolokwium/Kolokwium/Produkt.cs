f37abf57975cf141d9d949a8dc7d2c30550b0de4
student <student@DESKTOP-OT59EC7>
2020-01-25 14:42:45 +0100

    Oddaje kolokwium

---
+++ DrugieKolokwium/Kolokwium/Produkt.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kolokwium
+{
+    class Produkt
+    {
+        public string Nazwa { get; set; }
+        double Cena { get; set; }
+        string Opis { get; set; }
+        DateTime DataWaznosci { get; set; }
+        int Ilosc { get; set; }
+        public Produkt(string nazwa, double cena, string opis, DateTime dataWaznosci, int ilosc)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Opis = opis;
+            DataWaznosci = dataWaznosci;
+            Ilosc = ilosc;
+            WybranoProdukt += UruchomPodajnikEventHandler;
+            WybranoProdukt += WypiszKomunikatEventHandler;
+            WybranoProdukt += SprzedanoProduktEventHandler;
+        }
+
+        public string Dekonstruktor()
+        {
+            return $"{Nazwa}, cena: {Cena}zl";
+        }
+
+        public string Dekonstruktor2()
+        {
+            if (DateTime.Now.AddDays(1) > DataWaznosci)
+            {
+                return "MNIEJ NIZ 1 DZIEN!";
+            }
+
+            else if (DateTime.Now.AddDays(7) > DataWaznosci)
+            {
+                return "mniej niz tydzien";
+            }
+
+            else
+            {
+                return $"{Nazwa}, cena:{Cena}zl, opis: {Opis}, data waznosci{DataWaznosci.ToShortDateString()}";
+            }
+        }
+
+        public event EventHandler WybranoProdukt;
+
+        public void ProduktSprzedany()
+        {
+            if (Ilosc > 0)
+            {
+                Ilosc--;
+                WybranoProdukt?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void UruchomPodajnikEventHandler(object sender, EventArgs args)
+        {
+            Console.WriteLine("Uruchomiono podajnik!");
+        }
+
+        private void WypiszKomunikatEventHandler(object sender, EventArgs args)
+        {
+            Console.WriteLine($"Wypisano komunikat {DateTime.Now}");
+        }
+
+        private void SprzedanoProduktEventHandler(object sender, EventArgs args)
+        {
+            Console.WriteLine($"Sprzedano produkt {Nazwa}");
+        }
+
+    }
+}
--- DrugieKolokwium/Kolokwium/Program.cs
+++ DrugieKolokwium/Kolokwium/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Kolokwium
 {
     class Program
     {
+        static private Random rand = new Random();
+
+        static DateTime LosujDate()
+        {
+            return new DateTime((long)rand.Next()*100000000);
+        }
+
         static void Main()
         {
+            var kolekcja = new List<DateTime>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                kolekcja.Add(LosujDate());
+            }
+
+            Console.WriteLine(LosujDate());
+
+            IEnumerable ienumerable = kolekcja;
+
+            var puszkaPepsi = new Produkt("Pepsi", 2.99, "zwykla puszka pepsi", new DateTime(2020, 12, 6), 3);
+
+            puszkaPepsi.ProduktSprzedany();
+
             Console.ReadKey();
         }
     }
